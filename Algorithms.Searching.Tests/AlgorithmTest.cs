d5792e394d726db64cd03d61b2efd0f63b6251f0
franz maurer <adnil997@wp.pl>
2017-10-21 19:18:50 +0200

    binary search

--- Algorithms.Searching.Tests/AlgorithmTest.cs
+++ Algorithms.Searching.Tests/AlgorithmTest.cs
@@ -12,18 +12,173 @@ namespace Algorithms.Searching.Tests
     public class AlgorithmTest
     {
         [Test]
-        public void LinearSearch_InputNotEmpty_ReturnsSortedCol()
+        public void LinearSearch_SearchedElementExists_ReturnsIndex()
         {
-
             //Arrange
             var collection = new[] { 3, 4, 5, 2, 3, 12, 1 };
             var algotihm = new Algorithm();
 
             //Act
-            var result=algotihm.LinearSearch(collection, 2);
+            var result = algotihm.LinearSearch(collection, 2);
 
             //Assert
             Assert.AreEqual(3, result);
         }
+
+        [Test]
+        public void LinearSearch_SearchedElementNotExists_ReturnsNull()
+        {
+            //Arrange
+            var collection = new[] { 3, 4, 5, 2, 3, 12, 1 };
+            var algotihm = new Algorithm();
+
+            //Act
+            var result = algotihm.LinearSearch(collection, 10);
+
+            //Assert
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void LinearSearch_CollectionIsNull_ReturnsNull()
+        {
+            //Arrange
+            int[] collection = null;
+            var algotihm = new Algorithm();
+
+            //Act
+            var result = algotihm.LinearSearch(collection, 10);
+
+            //Assert
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void LinearSearch_CollectionIsEmpty_ReturnsNull()
+        {
+            //Arrange
+            int[] collection = new int[] { };
+            var algotihm = new Algorithm();
+
+            //Act
+            var result = algotihm.LinearSearch(collection, 10);
+
+            //Assert
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void LinearSearch_CollectionWithOneElementElementExists_ReturnsIndex()
+        {
+            //Arrange
+            int[] collection = new int[] { 5 };
+            var algotihm = new Algorithm();
+
+            //Act
+            var result = algotihm.LinearSearch(collection, 5);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void LinearSearch_CollectionWithOneElementElementNotExists_ReturnsNull()
+        {
+            //Arrange
+            int[] collection = new int[] { 5 };
+            var algotihm = new Algorithm();
+
+            //Act
+            var result = algotihm.LinearSearch(collection, 10);
+
+            //Assert
+            Assert.AreEqual(null, result);
+        }
+
+        //-- binary search tests
+        [Test]
+        public void BinarySearch_SearchedElementExists_ReturnsIndex()
+        {
+            //Arrange
+            var collection = new[] { 3, 4, 5, 2, 3, 12, 1 };
+            var algotihm = new Algorithm();
+
+            //Act
+            var result = algotihm.BinarySearch(collection, 2);
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void BinarySearch_SearchedElementNotExists_ReturnsNull()
+        {
+            //Arrange
+            var collection = new[] { 3, 4, 5, 2, 3, 12, 1 };
+            var algotihm = new Algorithm();
+
+            //Act
+            var result = algotihm.BinarySearch(collection, 10);
+
+            //Assert
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void BinarySearch_CollectionIsNull_ReturnsNull()
+        {
+            //Arrange
+            int[] collection = null;
+            var algotihm = new Algorithm();
+
+            //Act
+            var result = algotihm.BinarySearch(collection, 10);
+
+            //Assert
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void BinarySearch_CollectionIsEmpty_ReturnsNull()
+        {
+            //Arrange
+            int[] collection = new int[] { };
+            var algotihm = new Algorithm();
+
+            //Act
+            var result = algotihm.BinarySearch(collection, 10);
+
+            //Assert
+            Assert.AreEqual(null, result);
+        }
+
+        [Test]
+        public void BinarySearch_CollectionWithOneElementElementExists_ReturnsIndex()
+        {
+            //Arrange
+            int[] collection = new int[] { 5 };
+            var algotihm = new Algorithm();
+
+            //Act
+            var result = algotihm.BinarySearch(collection, 5);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void BinarySearch_CollectionWithOneElementElementNotExists_ReturnsNull()
+        {
+            //Arrange
+            int[] collection = new int[] { 5 };
+            var algotihm = new Algorithm();
+
+            //Act
+            var result = algotihm.BinarySearch(collection, 10);
+
+            //Assert
+            Assert.AreEqual(null, result);
+        }
+
     }
 }
--- Algorithms.Searching/Algorithm.cs
+++ Algorithms.Searching/Algorithm.cs
@@ -9,7 +9,7 @@ namespace Algorithms.Searching
     public class Algorithm
     {
 
-        //
+        // computational complexity is linear O(N);
         public int? LinearSearch(int[] collection, int elementToFind)
         {
             if (collection == null || collection.Length == 0)
@@ -26,10 +26,43 @@ namespace Algorithms.Searching
             return index <= collection.Length - 1 ? (Nullable<int>)index : null;
         }
 
-        //
+        // computational complexity is linear O(log2(N));
         public int? BinarySearch(int[] collection, int elementToFind)
         {
+            if (collection == null || collection.Length == 0)
+            {
+                return null;
+            }
+
             collection.OrderBy(x => x);
+
+            var leftIndex = 0;
+            var rightIndex = collection.Length - 1;
+
+            while (leftIndex <= rightIndex)
+            {       
+                int middleIndex = (leftIndex + rightIndex) / 2;
+                if (collection[middleIndex] == elementToFind)
+                {
+                    return middleIndex;
+                }
+                else if (collection[middleIndex] > elementToFind)
+                {
+                    rightIndex = middleIndex-1;
+                }
+                else
+                {
+                    leftIndex = middleIndex+1;
+                }
+            }
+
+            return null;
+        }
+
+
+
+        public int? BinarySearchWithRecursion(int[] collection, int elementToFind)
+        {
 
             return null;
         }
