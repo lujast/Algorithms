c8a8df03ad6ed6d1b3d88cd4af7b9f281396fc02
franz maurer <adnil997@wp.pl>
2017-10-21 17:23:21 +0200

    Searching project added

---
+++ Algorithms.Searching/Algorithm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Searching
+{
+    public class Algorithm
+    {
+        public int? LinearSearch()
+        {
+
+            return null;
+        }
+    }
+}
